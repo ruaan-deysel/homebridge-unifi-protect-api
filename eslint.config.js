8fc6f77d68c6ffefe8800b14c2b43143a79bf8ce
Ruaan Deysel <ruaan.deysel@gmail.com>
2026-07-31 17:06:10 +1000

    fix: exclude .github/instructions from eslint scope

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,5 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   typescript: true,
-  ignores: ['dist', 'src/protect/schemas.ts', 'spec'],
+  ignores: ['dist', 'src/protect/schemas.ts', 'spec', '.github/instructions'],
 })
